851aeffa157cf3329caba731a6e4c08b7941e5c7
Michal <michal.rydzewski.03@gmail.com>
2025-07-07 09:46:39 +0200

    add eslint and ES6 support for jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,5 +18,19 @@ export default defineConfig([
       },
     },
   },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
   eslintConfigPrettier,
 ])
