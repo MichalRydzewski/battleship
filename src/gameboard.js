50eb8504fcd7a976a62fa4804e514ca6601a4f0f
Michal <michal.rydzewski.03@gmail.com>
2025-07-09 15:20:30 +0200

    fix calculateCoordinates() and createBoard()
    Both functions are now outside of Gameboard class:
    calculateCoordinates() now works on arrays instead of strings
    createBoard() now returns [X,Y] coords in growing X-first manner

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,17 +1,15 @@
 export class Gameboard {
-  createBoard() {
-    let arr = []
-    for (let i = 0; i < 10; i++) {
-      arr[i] = []
-      for (let j = 0; j < 10; j++) {
-        arr[i][j] = `[${i}][${j}]`
-      }
-    }
-    return arr
+  constructor() {
+    this.board = createBoard()
   }
 
-  placeShip() {
-    
+  placeShip(coordinates, slope, ship) {
+    const shipCoords = calculateCoordinates(coordinates, slope, ship.length)
+    for (let i = 0; i < shipCoords.length; i++) {
+      const thisCoord = shipCoords[i]
+      this.board[thisCoord[0]][thisCoord[1]] = "test"
+    }
+    return shipCoords
   }
 
   receiveAttack() {
@@ -22,4 +20,32 @@ export class Gameboard {
   // gameboards should keep track of missed shots so they can display them properly
 
   // gameboards should be able to report whether or not all their ships have been sunk
+}
+
+function createBoard() {
+  let board = []
+  for (let i = 0; i < 10; i++) {
+    board[i] = []
+    for (let j = 0; j < 10; j++) {
+      board[i][j] = [j, i]
+    }
+  }
+  return board
+}
+
+function calculateCoordinates(coordinates, slope, length) {
+  const shipCoords = []
+  const X = coordinates[0]
+  const Y = coordinates[1]
+
+  for (let i = 0; i < length; i++) {
+    if (slope === "|") {
+      const newCoords = [X, Y + i]
+      shipCoords.push(newCoords)
+    } else if (slope === "-") {
+      const newCoords = [X + i, Y]
+      shipCoords.push(newCoords)
+    } else throw "Wrong slope"
+  }
+  return shipCoords
 }
