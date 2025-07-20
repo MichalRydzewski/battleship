41511d6b3de6ab1d74cf26783ffa597017171b5b
Michal <michal.rydzewski.03@gmail.com>
2025-07-20 13:28:14 +0200

    adjust sunken state
    It's now a result of outside function,
    hit method now updates sunken state.

--- src/ship.js
+++ src/ship.js
@@ -2,15 +2,16 @@ export class Ship {
   constructor(length) {
     this.length = length
     this.timesHit = 0
-    this.sunken = false
+    this.sunken = isSunk(this)
   }
 
   hit() {
     this.timesHit++
+    this.sunken = isSunk(this)
   }
+}
 
-  isSunk() {
-    if (this.timesHit >= this.length) return (this.sunken = true)
-    else return (this.sunken = false)
-  }
+function isSunk(ship) {
+  if (ship.timesHit >= ship.length) return true
+  else return false
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,16 +1,20 @@
 import { Ship } from "../src/ship.js"
 
 const testShip = new Ship(3)
+const testShip2 = new Ship(2)
+
+testShip.hit()
+testShip.hit()
+
+testShip2.hit()
+testShip2.hit()
 
 test("increases the number of hits in ship", () => {
-  testShip.hit()
-  testShip.hit()
-  testShip.hit()
-  testShip.hit()
   expect(testShip.timesHit).toBeGreaterThan(0)
 })
 
 test("ship is sunk when number of hits equals or is greater than its length", () => {
-  testShip.isSunk()
-  expect(testShip.sunken).toBe(true)
+  expect(testShip.sunken).toBe(false)
+
+  expect(testShip2.sunken).toBe(true)
 })
