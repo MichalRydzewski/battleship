5b1dfdcec15b52dca07b2c67eabaf122480b49b3
Michal <michal.rydzewski.03@gmail.com>
2025-08-01 11:52:10 +0200

    generate ships on board, adjust UX
    Added ship rendering, github footer, background image and its author.
    Ships are rendering, but function responsible for rendering them on proper spots isn't working correctly.

--- src/DOM.js
+++ src/DOM.js
@@ -1,19 +1,24 @@
 import { setUpShips, validCoordsArr } from "./game.js"
 
-setUpShips()
-console.log(validCoordsArr)
-
 export function loadDOM() {
   const board1 = document.querySelector(".board-1")
   loadBoard(board1)
   const board2 = document.querySelector(".board-2")
   loadBoard(board2)
+
+  setUpShips()
+  const sortedShipsCoordsArr = validCoordsArr.sort((a, b) => a[0] - b[0])
+  sortedShipsCoordsArr.forEach(coord => {
+    board1.querySelector(`.cell-${coord[0]}-${coord[1]}`).classList.add("ship")
+  })
 }
 
 function loadBoard(board) {
-  for (let i = 1; i <= 100; i++) {
-    const cell = document.createElement("div")
-    cell.classList.add("cell", `cell-${i}`)
-    board.appendChild(cell)
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("div")
+      cell.classList.add("cell", `cell-${i}-${j}`)
+      board.appendChild(cell)
+    }
   }
 }
